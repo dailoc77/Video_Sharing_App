3e6cc4fa60f5da36c769dd9dc8c144bfa92e9d7b
dailoc77 <truongdailoc.dev@gmail.com>
2024-10-18 20:20:36 +0700

    done Post Video Screen

--- screens/Post_Video_Screen.js
+++ screens/Post_Video_Screen.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { View, StyleSheet, Image, Dimensions ,ScrollView} from "react-native";
+import { View, StyleSheet, Image, Dimensions, ScrollView } from "react-native";
 import { TextInput, Text } from "react-native";
 
 const Post_Video_Screen = ({ navigation }) => {
-
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imageContainer}>
@@ -39,42 +38,84 @@ const Post_Video_Screen = ({ navigation }) => {
       </View>
 
       <View style={styles.itemContainer1}>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Add_hashtag.png")} />
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Tag_5.png")} />
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Add_hashtag.png")}
+        />
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Tag_5.png")}
+        />
       </View>
 
       <View style={styles.itemContainer2}>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Tag_someone.png")}/>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Tag_6.png")}/>
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Tag_someone.png")}
+        />
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Tag_6.png")}
+        />
       </View>
 
       <View style={styles.itemContainer3}>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Comments.png")}/>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Switch_1.png")}/>
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Comments.png")}
+        />
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Switch_1.png")}
+        />
       </View>
 
       <View style={styles.itemContainer3}>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Who_can_watch.png")}/>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Dropdown_Button_1.png")}/>
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Who_can_watch.png")}
+        />
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Dropdown_Button_1.png")}
+        />
       </View>
 
       <View style={styles.itemContainer1}>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Also_poston.png")}/>
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Also_poston.png")}
+        />
       </View>
 
       <View style={styles.itemContainer3}>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Facebook.png")}/>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Switch_2.png")}/>
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Facebook.png")}
+        />
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Switch_2.png")}
+        />
       </View>
 
       <View style={styles.itemContainer3}>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Twitter.png")}/>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Switch_3.png")}/>
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Twitter.png")}
+        />
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Switch_3.png")}
+        />
       </View>
 
       <View style={styles.itemContainer3}>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Instagram.png")}/>
-        <Image source={require("../assets/Data/CreateVideoPostVideo/Switch_4.png")}/>
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Instagram.png")}
+        />
+        <Image
+          source={require("../assets/Data/CreateVideoPostVideo/Switch_4.png")}
+        />
+      </View>
+      <View style={styles.itemContainer3}>
+        <TouchableOpacity>
+          <Image
+            source={require("../assets/Data/CreateVideoPostVideo/Button_17.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={require("../assets/Data/CreateVideoPostVideo/Button_18.png")}
+          />
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
@@ -83,54 +124,54 @@ const Post_Video_Screen = ({ navigation }) => {
 const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-    },
-    imageContainer: {
-      alignItems: "center",
-      justifyContent: "center",
-      paddingVertical: 10,
-    },
-    image: {
-      width: "100%", // Đảm bảo chiếm hết chiều rộng
-      aspectRatio: 1, // Đảm bảo hình ảnh giữ tỷ lệ 1:1
-    },
-    inputContainer: {
-      marginBottom: 30, // Khoảng cách giữa các input
-      paddingHorizontal: 20,
-      marginTop: -10, // Điều chỉnh khoảng cách giữa ảnh và input
-    },
-    inputLabel: {
-      fontSize: 16,
-      fontWeight: "bold",
-      marginBottom: 5,
-    },
-    input: {
-      height: 40, // Chiều cao cho input tiêu đề
-      borderColor: "rgba(0, 0, 0, 0.1)",
-      borderWidth: 1,
-      borderRadius: 5,
-      paddingHorizontal: 10,
-      backgroundColor: "#F5F5F5",
-    },
-    itemContainer1: {
-      marginBottom: 20,
-      paddingHorizontal: 20,
-    },
-    itemContainer2: {
-      flexDirection: 'row',
-      marginBottom: 20,
-      paddingHorizontal: 20,
-      alignItems: 'center',
-    },
-    itemContainer3: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginBottom: 20,
-      paddingHorizontal: 20,
-      alignItems: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  imageContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 10,
+  },
+  image: {
+    width: "100%", // Đảm bảo chiếm hết chiều rộng
+    aspectRatio: 1, // Đảm bảo hình ảnh giữ tỷ lệ 1:1
+  },
+  inputContainer: {
+    marginBottom: 30, // Khoảng cách giữa các input
+    paddingHorizontal: 20,
+    marginTop: -10, // Điều chỉnh khoảng cách giữa ảnh và input
+  },
+  inputLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  input: {
+    height: 40, // Chiều cao cho input tiêu đề
+    borderColor: "rgba(0, 0, 0, 0.1)",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    backgroundColor: "#F5F5F5",
+  },
+  itemContainer1: {
+    marginBottom: 20,
+    paddingHorizontal: 20,
+  },
+  itemContainer2: {
+    flexDirection: "row",
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    alignItems: "center",
+  },
+  itemContainer3: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    paddingHorizontal: 40,
+    alignItems: "center",
+  },
+});
 
 export default Post_Video_Screen;
